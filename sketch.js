2673c4bd483cdb58ee4273b203bb87f174bcc93b
sparlingaidan <sparliai@dukes.jmu.edu>
2024-12-20 22:10:13 -0500

    ph
    phgrid

--- sketch.js
+++ sketch.js
@@ -1,9 +1,31 @@
+const imgWidth = 640;
+const imgHeight = 480;
+
 function setup() {
-  createCanvas(640, 580);
+  createCanvas(imgWidth, 940);
   imgs = loadImage('pic/test.jpg');
 }
 
+function drawpH(){
+  fill('white');
+  stroke('blue');
+  strokeWeight(2);
+  text("6.2", 0, imgHeight + 15);
+  text("5.8", 0, imgHeight + 55);
+  text("5.3", 0, imgHeight + 105);
+  for (i = imgHeight + 10; i < imgHeight + 110; i = i + 10){
+    line(20, i, imgWidth, i);
+  }
+  for ( i = 20; i < imgWidth; i = i + 40){
+    line(i, imgHeight +10, i, imgHeight + 100);
+  }
+  strokeWeight(2);
+  stroke('black');
+  line(0, imgHeight, imgWidth, imgHeight);
+}
+
 function draw() {
-  background('#033000');
-  image(imgs, 0, 0); 
+  background('pink');
+  image(imgs, 0, 0);
+  drawpH(); 
 }
