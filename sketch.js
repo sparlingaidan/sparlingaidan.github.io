501dc34310c0fa64c664c9bb8b71ce5f11796235
sparlingaidan <sparliai@dukes.jmu.edu>
2024-12-23 12:46:14 -0500

    Play  button
    Play button

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 const imgWidth = 640;
 const imgHeight = 480;
 let len;
-let imgs = []
+let imgs = [];
+let currentImg = 0;
 
 function preload(){
   len = loadStrings('/len.txt');
@@ -15,7 +16,7 @@ function setup() {
     temp = loadImage(fileName);
     imgs.push(temp);
   }
-  console.log(fileName);
+  currentImg = len[0].length -1;
 }
 
 function drawpH(){
@@ -63,10 +64,36 @@ function drawWater(){
   }
 }
 
+function drawPlay(){
+  fill('pink');
+  rect(imgWidth - 60, 10, 50, 27);
+  stroke('black');
+  textSize(21);
+  text("Play", imgWidth - 57, 29);
+  textSize(10);
+}
+
 function draw() {
   background('pink');
-  image(imgs[len[0].length -1], 0, 0);
+  image(imgs[currentImg], 0, 0);
+  drawPlay();
   drawpH();
   drawCanopy();
   drawWater();
+  updateAll();
+}
+
+function mouseClicked() {
+  if((mouseX > 570) & (mouseY < 50)){
+    currentImg = 0;
+  }
+}
+
+function updateAll(){
+  if (frameCount % 25 == 0){
+    currentImg ++;
+  }
+  if (currentImg >= (4) ){
+    currentImg = len[0].length -1;
+  }
 }
